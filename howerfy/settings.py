af7549ba26d35f0ee3a513fe19c8c94e03f3bc6c
Florian Sieren <florian@Flo-pro.local>
2021-04-02 17:54:02 +0200

    WIP: eMail feature

--- howerfy/settings.py
+++ howerfy/settings.py
@@ -143,5 +143,5 @@ STATIC_URL = '/static/'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_HOST_USER = 'Howerfy@gmail.com'
-EMAIL_HOST_PASSWORD = os.environ['EMAIL_PASSWORD']
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_PASSWORD', 'default']
 EMAIL_USE_TLS = True
