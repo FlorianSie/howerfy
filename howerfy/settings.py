2e255a310ef817f8fec854ebed858e0e809786b9
Florian Sieren <florian@Flo-pro.local>
2021-04-02 18:10:51 +0200

    WIP: eMail feature

--- howerfy/settings.py
+++ howerfy/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'qpt+sj-&7$hq8+c=ne0j39%(*$gl@38t#4s+f%%g)0@&4$vrjs'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
